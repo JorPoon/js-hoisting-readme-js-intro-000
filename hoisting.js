5a0a65f1f079d895155ddc937ba3d47842af8b4a
Jor Ming Poon <jormpoon@gmail.com>
2018-08-13 22:45:26 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -21,14 +21,14 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  // var name = "Cricky";
+   var name = "Cricky";
 
-
+  sayMy();
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
 
   }
-  sayMy();
+  
 }
